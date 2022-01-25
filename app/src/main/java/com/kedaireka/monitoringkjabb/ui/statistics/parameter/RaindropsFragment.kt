e29d825b17c27834571b00243a0b7a8b7a91b2ea
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-01-25 13:51:58 +0700

    Statistics Fragment Raindrops

--- app/src/main/java/com/kedaireka/monitoringkjabb/ui/statistics/parameter/RaindropsFragment.kt
+++ app/src/main/java/com/kedaireka/monitoringkjabb/ui/statistics/parameter/RaindropsFragment.kt
@@ -1,14 +1,19 @@
 package com.kedaireka.monitoringkjabb.ui.statistics.parameter
 
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.*
+import android.provider.Settings
 import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.util.Pair
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.github.mikephil.charting.charts.LineChart
@@ -17,11 +22,14 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.ValueFormatter
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.firebase.Timestamp
 import com.kedaireka.monitoringkjabb.R
 import com.kedaireka.monitoringkjabb.databinding.FragmentRaindropsBinding
 import com.kedaireka.monitoringkjabb.model.Sensor
+import com.kedaireka.monitoringkjabb.ui.detail.DetailSensorActivity
 import com.kedaireka.monitoringkjabb.utils.ExcelUtils
+import com.kedaireka.monitoringkjabb.utils.RaindropsMapper.Companion.RAINDROPS_DICT
 import java.util.*
 import java.util.concurrent.Executors
 import kotlin.collections.ArrayList
@@ -29,10 +37,14 @@ import kotlin.collections.ArrayList
 class RaindropsFragment : Fragment() {
 
     private lateinit var raindropsFragmentViewModel: RaindropsFragmentViewModel
-    private lateinit var allRecords: ArrayList<Sensor>
+    private lateinit var recordsInRange: ArrayList<Sensor>
 
     private var _binding: FragmentRaindropsBinding? = null
     private val binding get() = _binding!!
+
+    private var max = 0.0
+    private var min = 0.0
+    private var avg = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,14 +57,43 @@ class RaindropsFragment : Fragment() {
 
         val sensor = getLatestSensor()
         raindropsFragmentViewModel.getDORecord(sensor)
-        raindropsFragmentViewModel.getAllDORecord(sensor)
-        raindropsFragmentViewModel.allRecord.observe(viewLifecycleOwner, { result ->
-            allRecords = result
+        raindropsFragmentViewModel.getThresholdsData(sensor)
+
+        raindropsFragmentViewModel.avg.observe(viewLifecycleOwner, { result ->
+            avg = result
+            val value = getString(RAINDROPS_DICT[avg.toInt()]!!)
+            binding.tvAvg.text = value
+        })
+
+        raindropsFragmentViewModel.max.observe(viewLifecycleOwner, {
+            max = it
+            val value =
+                "Max: ${getString(RAINDROPS_DICT[max.toInt()]!!)} | Min: ${getString(RAINDROPS_DICT[min.toInt()]!!)}"
+            binding.tvMaxMin.text = value
+        })
+
+        raindropsFragmentViewModel.min.observe(viewLifecycleOwner, {
+            min = it
+            val value =
+                "Max: ${getString(RAINDROPS_DICT[max.toInt()]!!)} | Min: ${getString(RAINDROPS_DICT[min.toInt()]!!)}"
+            binding.tvMaxMin.text = value
         })
 
         raindropsFragmentViewModel.records.observe(viewLifecycleOwner, { result ->
             val lineChart = binding.lineChart
             setDOLineChart(lineChart, result)
+        })
+
+        raindropsFragmentViewModel.thresholds.observe(viewLifecycleOwner, {
+            val upper = it["upper"]?.toDouble()!!
+            val lower = it["lower"]?.toDouble()!!
+
+            if (avg in lower..upper) {
+                binding.tvStatus.text = getString(R.string.status_good)
+            } else {
+                binding.tvStatus.text = getString(R.string.status_bad)
+                binding.cardStatus.setCardBackgroundColor(resources.getColor(R.color.yellow))
+            }
         })
 
         raindropsFragmentViewModel.isLoading.observe(viewLifecycleOwner, {
@@ -68,20 +109,80 @@ class RaindropsFragment : Fragment() {
         val executor = Executors.newSingleThreadExecutor()
         val handler = Handler(Looper.getMainLooper())
         binding.button.setOnClickListener {
-            Toast.makeText(this.requireContext(), "Saving Data", Toast.LENGTH_SHORT).show()
-
-            executor.execute {
-                val workbook = ExcelUtils.createWorkbook(allRecords)
-                ExcelUtils.createExcel(
-                    this.requireContext().applicationContext,
-                    workbook,
-                    sensor
+            // Check storage permission before download
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                checkPermission(
+                    Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                    DetailSensorActivity.MANAGE_STORAGE_PERMISSION_CODE
                 )
 
-                handler.post {
-                    Toast.makeText(this.requireContext(), "Data Saved", Toast.LENGTH_SHORT).show()
+                if (!Environment.isExternalStorageManager()) {
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+                    val uri = Uri.fromParts("package", requireContext().packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
                 }
+
+            } else {
+                checkPermission(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    DetailSensorActivity.STORAGE_PERMISSION_CODE
+                )
             }
+
+            val dateRangePicker = MaterialDatePicker.Builder.dateRangePicker()
+                .setTitleText("Select dates")
+                .setSelection(
+                    Pair(
+                        MaterialDatePicker.thisMonthInUtcMilliseconds(),
+                        MaterialDatePicker.todayInUtcMilliseconds()
+                    )
+                )
+                .build()
+
+            dateRangePicker.addOnPositiveButtonClickListener { time ->
+                // Generate Data
+                Toast.makeText(requireContext(), "Saving Data", Toast.LENGTH_SHORT).show()
+
+                raindropsFragmentViewModel.getSensorRecordInRange(
+                    sensor,
+                    time.first / 1000,
+                    time.second / 1000
+                )
+                raindropsFragmentViewModel.sensorRecordInRange.observe(requireActivity(), {
+                    recordsInRange = it
+
+                    if (recordsInRange.isNotEmpty()) {
+                        executor.execute {
+                            val workbook = ExcelUtils.createWorkbook(recordsInRange)
+                            ExcelUtils.createExcel(
+                                requireContext().applicationContext,
+                                workbook,
+                                sensor
+                            )
+
+                            handler.post {
+                                Toast.makeText(
+                                    requireContext(),
+                                    getString(R.string.data_saved),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            getString(R.string.saving_failed),
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+
+                })
+            }
+
+            dateRangePicker.show(requireActivity().supportFragmentManager, "DetailSensorActivity")
         }
 
         return root
@@ -93,7 +194,7 @@ class RaindropsFragment : Fragment() {
         val id = "raindrops"
         val name = "Raindrops"
         val value = "6.3"
-        val unit = "mg/l"
+        val unit = ""
         val createdAt = Timestamp(Date())
         val iconUrl = "url"
 
@@ -136,5 +237,26 @@ class RaindropsFragment : Fragment() {
         lineChart.data = data
         lineChart.setScaleEnabled(false)
 
+    }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(),
+                permission
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            // Requesting permission
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(permission),
+                requestCode
+            )
+        } else {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.permission_already_granted),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
